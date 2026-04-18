5cae441dedb0e878eb330d87a0c2baaa7a2900a0
Caseyy43 <46385822+Caseyy43@users.noreply.github.com>
2026-04-18 16:58:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
 const CACHE = 'visitpro-v1';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/visitpro/', '/visitpro/index.html'])));
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(['/visitpro/', '/visitpro/index.html'])
+    )
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
